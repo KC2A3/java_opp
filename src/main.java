49a9a97c83c39ba5d0d74ab20bda600583d0bf5a
KC2A3 <947496351@qq.com>
2019-02-24 23:18:45 +0800

    Call the method to test in Main

--- src/main.java
+++ src/main.java
@@ -4,11 +4,15 @@ import model.Teacher;
 
 public class main {
   public static void main(String[] args) {
+    Klass klassOne = new Klass(1);
     Klass klassTwo = new Klass(2);
+    Klass klassThree = new Klass(3);
+    Student mike = new Student(4564,"mike",22,klassOne);
     Student Tom = new Student(9527, "Tom", 21, klassTwo);
     klassTwo.assignLeader(Tom);
     System.out.println(Tom.introduce());
-    Teacher jack = new Teacher(23,"jack",21,new Klass[]{});
+    Teacher jack = new Teacher(23,"jack",21,new Klass[]{klassOne,klassThree});
     System.out.println(jack.introduce());
+    System.out.println(jack.isTeaching(Tom));
   }
 }
