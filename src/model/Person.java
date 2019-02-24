c9f244c72077da3a29e3542c934d05fc5e90da86
KC2A3 <947496351@qq.com>
2019-02-24 20:44:44 +0800

    Write a Person class with id, name, and age attributes.

---
+++ src/model/Person.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Person {
+  private int id;
+  private String name;
+  private int age;
+
+  public Person(int id, String name, int age) {
+    this.age = age;
+    this.id = id;
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String introduce() {
+    return "My name is " + this.name + ". I am " + this.age + " years old.";
+  }
+}
