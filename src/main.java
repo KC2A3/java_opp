809542ce7d777d5d314eed42f0e13d601eb36337
KC2A3 <947496351@qq.com>
2019-02-24 21:20:23 +0800

    Write a main method

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import model.Klass;
+import model.Student;
+
+public class main {
+    public static void main(String[] args){
+        Klass klassTwo = new Klass(2);
+        Student Tom = new Student(9527,"Tom",21,klassTwo);
+        System.out.println(Tom.introduce());
+    }
+}
